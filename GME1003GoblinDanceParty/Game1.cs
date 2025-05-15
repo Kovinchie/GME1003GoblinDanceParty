fdb41e7663a990582f2f00bc702423a5f5f755ef
Kovinchie <kovencb1@gmail.com>
2025-05-15 19:58:05 -0400

    added own textures
    THE SCALE IS WEIRDDDDD gonna have to resize the image itself because it would be the easiest fix

--- GME1003GoblinDanceParty/Game1.cs
+++ GME1003GoblinDanceParty/Game1.cs
@@ -13,15 +13,20 @@ namespace GME1003GoblinDanceParty
         private SpriteBatch _spriteBatch;
 
         //Declare some variables/
-        private int _numStars;          //how many stars?
-        private List<int> _starsX;      //list of star x-coordinates
-        private List<int> _starsY;      //list of star y-coordinates
-        private List<Color> _starColor; //KOVINCHIE CHANGE list of star colors 
-        private List<float> _starScale; //KOVINCHIE CHANGE list of star scale
+        private int _numStars;                  //how many stars?
+        private List<int> _starsX;              //list of star x-coordinates
+        private List<int> _starsY;              //list of star y-coordinates
+        private List<Color> _starColor;         //KOVINCHIE CHANGE list of star colors 
+        private List<float> _starScale;         //KOVINCHIE CHANGE list of star scale
         private List<float> _starTransparency;  // KOVINCHIE CHNAGE list of transparency
         private List<float> _starRotation;      // KOVINCHIE CHANGE list of rotation
 
-        private Texture2D _starSprite;  //the sprite image for our star
+        private List<Texture2D> _piece;         //KOVINCHIE CHANGE list for image
+        private Texture2D _tetrisBackround;     //KOVINCHIE CHANGE backround for the monogame
+
+        private Texture2D _starSprite;          //the sprite image for our star
+
+        private Random _rng;                    //for all our random number needs
 
         //***This is for the goblin. Ignore it.
         Goblin goblin;
@@ -37,7 +42,7 @@ namespace GME1003GoblinDanceParty
 
         protected override void Initialize()
         {
-            Random _rng = new Random();             //KOVINCHIE CHANGE i made it into a decloration instead of an assign
+            _rng = new Random();                    //finish setting up our Random 
             _numStars = _rng.Next(100,301);         //KOVINCHIE CHANGE i made this into a random number between 100 and 300
             _starsX = new List<int>();              //stars X coordinate
             _starsY = new List<int>();              //stars Y coordinate
@@ -45,18 +50,21 @@ namespace GME1003GoblinDanceParty
             _starScale = new List<float>();         //KOVINCHIE CHANGE now is a list of floats
             _starTransparency = new List<float>();  //KOVINCHIE CHANGE now is a list of floats
             _starRotation = new List<float>();      //KOVINCHIE CHANGE now is a list of floats
+            _piece = new List<Texture2D>();         //KOVINCHIE CHANGE now is a list of textures
 
             //use a separate for loop for each list - for practice
+            //ToDo: List of scale values
+
             //List of X coordinates
             for (int i = 0; i < _numStars; i++) 
             { 
-                _starsX.Add(_rng.Next(0, 801)); //all star x-coordinates are between 0 and 801
+                _starsX.Add(_rng.Next(0,801)); //all star x-coordinates are between 0 and 801
             }
 
             //List of Y coordinates
             for (int i = 0; i < _numStars; i++)
             {
-                _starsY.Add(_rng.Next(0, 481)); //all star y-coordinates are between 0 and 480
+                _starsY.Add(_rng.Next(0,481)); //all star y-coordinates are between 0 and 480
             }
 
             //ToDo: List of Colors
@@ -65,10 +73,9 @@ namespace GME1003GoblinDanceParty
                 _starColor.Add(new Color(128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129), 128 + _rng.Next(0, 129)));
             }
 
-            //ToDo: List of scale values
             for (int i = 0; i < _numStars; ++i)
             {
-                _starScale.Add(_rng.Next(50, 100) / 200f);
+                _starScale.Add(_rng.Next(50, 100) / 200f); // BIGGER
             }
 
             //ToDo: List of transparency values f
@@ -90,13 +97,18 @@ namespace GME1003GoblinDanceParty
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //load out star sprite
+            //load out sprites
             _starSprite = Content.Load<Texture2D>("starSprite");
 
+            _tetrisBackround = Content.Load<Texture2D>("Tetris");                   //KOVINCHIE BACKROUND
+            for (int i = 0; i < _numStars; i++)
+            {
+                _piece.Add(Content.Load<Texture2D>("piece" + _rng.Next(1, 7)));     //KOVINCHIE PIECES
+            }
 
             //***This is for the goblin. Ignore it for now.
-            goblin = new Goblin(Content.Load<Texture2D>("goblinIdleSpriteSheet"), 400, 400);
-            music = Content.Load<Song>("chiptune");
+            goblin = new Goblin(Content.Load<Texture2D>("Gamer"), 400, 400);        //KOVINCHIE GAMER
+            music = Content.Load<Song>("chiptune");                                 //
             
             //if you're tired of the music player, comment this out!
             MediaPlayer.Play(music);
@@ -123,12 +135,14 @@ namespace GME1003GoblinDanceParty
             _spriteBatch.Begin();
 
             //it would be great to have a background image here! 
-            //you could make that happen with a single Draw statement.
+
+            _spriteBatch.Draw(Content.Load<Texture2D>("Tetris"), new Vector2(0, 0), null, Color.White, 3.14f,
+                new Vector2(_tetrisBackround.Width, _tetrisBackround.Height), 2f, SpriteEffects.None, 0);       //KOVINCHIE BACKROUND DRAW
 
             //this is where we draw the stars...
             for (int i = 0; i < _numStars; i++) 
             {
-                _spriteBatch.Draw(_starSprite, 
+                _spriteBatch.Draw(_piece[i], 
                     new Vector2(_starsX[i], _starsY[i]),    //set the star position
                     null,                                   //ignore this
                     _starColor[i] * _starTransparency[i],         //set colour and transparency
